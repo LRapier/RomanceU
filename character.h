579e242440863fe86a241633e1314c63a8b4359c
gonzah19 <gonzah19@my.erau.edu>
2023-11-15 13:30:18 -0700

    Create character.h
    man.h, woman.h, and the class definitions have been combined into one .h file to get the setPoints() function to work

---
+++ character.h
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+class Character
+{
+   public:
+      string name;
+      friend ostream& operator<<(ostream &os, const Character &mc)
+      {
+         os<<mc.name;
+         return os;
+      }
+};
+class MainCharacter : public Character
+{
+   private:
+      int points = 0;
+      //char grade;
+   public: 
+      MainCharacter()
+      {}
+      void setName()
+      {
+         cout<<"What's your name?: ";
+         cin>>name;
+      }
+      void setPoints(int p)
+      {
+         points += p;
+      }
+      int getPoints()
+      {
+         return points;
+      }
+};
+class Man : public Character
+{
+   public:
+      Man()
+      {
+         name = "Justin Guy";
+      }
+};
+class Woman : public Character
+{
+   public:
+      Woman()
+      {
+         name = "Girlboss";
+      }
+};
+
+void ManInteractionOne(MainCharacter player);
+void ManInteractionTwo(MainCharacter player);
+void ManInteractionThree(MainCharacter player);
+void ManInteractionFour(MainCharacter player);
+void ManInteractionFive(MainCharacter player);
+
+void WomanInteractionOne(MainCharacter player);
+void WomanInteractionTwo(MainCharacter player);
+void WomanInteractionThree(MainCharacter player);
+void WomanInteractionFour(MainCharacter player);
+void WomanInteractionFive(MainCharacter player);
